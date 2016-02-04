b0c62fbaaa94cd4a9c15a0030bcbac60a148c4e5
Paige Hetherington <herbmama@gmail.com>
2016-02-04 15:38:08 -0500

    hw#4 initial import

--- src/com/company/ATM.java
+++ src/com/company/ATM.java
@@ -2,6 +2,7 @@ package com.company;
 
 import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class ATM {
@@ -10,12 +11,36 @@ public class ATM {
 
     public static void main(String[] args) throws Exception {
 
+        HashMap<String, Double> accounts = new HashMap();
+       while (true) {
+           Customer hustler = new Customer();
+           hustler.chooseName();
 
-//test for git
-        Customer hustler = new Customer();
-        hustler.chooseName();
-        hustler.chooseTodo();
+           if (!accounts.containsKey(hustler.name)) {
+               System.out.println("Would you like to create an account?");
+               String answer = scanner.nextLine();
+               if (answer.equalsIgnoreCase("y")) {
+                   accounts.put(hustler.name, hustler.balance);
+               } else {
+                   hustler.chooseName();
+               }
+           }
+           else {
+               hustler.balance = accounts.get(hustler.name);
+               System.out.println("Your balance is" + hustler.balance);
+           }
+           hustler.chooseTodo();
+           accounts.put(hustler.name, hustler.balance); //this puts info into hashmap after customer makes a todo choice
+           if (hustler.todo.equalsIgnoreCase("Cancel")) {
+               break;
+           }
+           if (hustler.todo.equalsIgnoreCase("Remove account")) {
+               accounts.remove(hustler.name);
+           }
+       }
+
 
     }
+
 
 }
